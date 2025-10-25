0e12e1de5cf4703eb033be503a40010905a4f610
Lemirq <sharmavihaan190@gmail.com>
2025-10-24 21:56:07 -0400

    Swift app

---
+++ swift-app/janusai/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  janusai
+//
+//  Created by Vihaan Sharma on 2025-10-24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ swift-app/janusai/janusaiApp.swift
@@ -0,0 +1,17 @@
+//
+//  janusaiApp.swift
+//  janusai
+//
+//  Created by Vihaan Sharma on 2025-10-24.
+//
+
+import SwiftUI
+
+@main
+struct janusaiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
